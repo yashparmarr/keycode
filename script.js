bbc3596b77edb154e15b5e3de75b5f6fde98c6d0
Yash Parmar <yash20545@gmail.com>
2025-01-17 15:52:19 +0530

    added scroll

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-let keyPressed = document.getElementById('keyPressed');
+let keyPressed = document.getElementById("keyPressed");
+let history = document.getElementById("history");
+let keyCode = document.getElementById("keyCode");
 
-let keyCode = document.getElementById('keyCode');
+let sound = new Audio("key-press.mp3");
+sound.preload = "auto";
 
-document.addEventListener('keydown',(e) => {
-    e.preventDefault();
-    let keyPress = e.key ;
-    let keycd = e.keyCode ;
-    pressing(keyPress,keycd);
+document.addEventListener("keypress", (e) => {
+  e.preventDefault();
+  let keyPress = e.key;
+  let keycd = e.keyCode;
+  pressing(keyPress, keycd);
 });
 
-let pressing = (keyPress,keycd) => {
-    keyPressed.innerHTML = `You Pressed <span style="color:rgb(5, 133, 90);;">${keyPress}</span>`;
-    keyCode.innerText = keycd ;
-    keyCode.style.display = 'block';
-}
+let pressing = (keyPress, keycd) => {
+  sound.currentTime = 0;
+  sound.play();
+  keyPressed.innerHTML = `You Pressed <span style="color:rgb(5, 133, 90);">${keyPress}</span>`;
+  keyCode.innerText = keycd;
+  keyCode.style.display = "block";
+  history.style.display = "flex";
+  let para = document.createElement("p");
+  para.innerText = `Key ${keyPress} , Code ${keycd}`;
+  para.classList.add("para");
+  history.appendChild(para);
+  history.scrollLeft = history.scrollWidth;
+  console.log(history);
+};
